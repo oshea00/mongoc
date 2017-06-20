03d4f240bdbcc31b8e323bf64905d19bea2b28c3
Mike OShea <michaelo@attinc.com>
2017-06-20 09:58:50 -0700

    init

---
+++ mongoc/Account.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mongoc
+{
+    public class Account
+    {
+        public ObjectId _id { get; set; }
+        public string AccountCode { get; set; }
+        public string AccountName { get; set; }
+        public string AccountTypeCode { get; set; }
+        public DateTime AccountOpened { get; set; }
+        public double AccountBalance { get; set; }
+        public override string ToString()
+        {
+            return $"{AccountCode} {AccountName} {AccountTypeCode} {AccountOpened.ToShortDateString()} {AccountBalance}";
+        }
+    }
+}
---
+++ mongoc/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace mongoc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MongoClient _client;
+        public MainWindow()
+        {
+            InitializeComponent();
+            var settings = new MongoClientSettings();
+            settings.ServerSelectionTimeout = TimeSpan.FromSeconds(5);
+            
+            _client = new MongoClient(settings);
+        }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var db = _client.GetDatabase("angular");
+                var accounts = db.GetCollection<Account>("accounts");
+                var all = accounts.Find<Account>(new BsonDocument()).ToList();
+                lstAccounts.ItemsSource = all;
+            }
+            catch (TimeoutException ex)
+            {
+                MessageBox.Show("Timed Out");
+            }
+
+
+        }
+    }
+}
