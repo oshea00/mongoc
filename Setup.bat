43d9c41af8a38389c99bd91ab0e064bc000a2ff9
Mike OShea <michaelo@attinc.com>
2017-06-20 14:05:43 -0700

    added setup.bat and setup.js

---
+++ Setup.bat
@@ -0,0 +1,6 @@
+﻿@no echo
+md .\data
+start mongod --dbpath .\data
+mongo localhost:27017 setup.js
+
+
--- mongoc/MainWindow.xaml.cs
+++ mongoc/MainWindow.xaml.cs
@@ -2,19 +2,8 @@
 using MongoDB.Bson;
 using MongoDB.Driver;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace mongoc
 {
@@ -28,7 +17,8 @@ namespace mongoc
         {
             InitializeComponent();
 
-            _server = new MongoServerHelper(@"c:\mongo\bin\mongod.exe", @"c:\projects\angularExample\data\db");
+            _server = new MongoServerHelper(@"C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe", 
+                                            @"c:\projects\mongoc\data");
             var settings = new MongoClientSettings();
             settings.ServerSelectionTimeout = TimeSpan.FromSeconds(5);
             if (_server.StartServer(settings) == false)
@@ -39,29 +29,21 @@ namespace mongoc
 
         private void btnFind_Click(object sender, RoutedEventArgs e)
         {
-            if (_server.ProcessStarted)
+            try
             {
-                try
-                {
-                    var db = _server.GetDatabase("angular");
-                    var accounts = db.GetCollection<Account>("accounts");
-                    var all = accounts.Find<Account>(new BsonDocument()).ToList();
-                    lstAccounts.ItemsSource = all;
-                }
-                catch (TimeoutException)
-                {
-                    MessageBox.Show("Timed Out", "Error");
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message, "Error");
-                }
+                var db = _server.GetDatabase("angular");
+                var accounts = db.GetCollection<Account>("accounts");
+                var all = accounts.Find<Account>(new BsonDocument()).ToList();
+                lstAccounts.ItemsSource = all;
             }
-            else
+            catch (TimeoutException)
             {
-                MessageBox.Show("Mongod server process not started.", "Error");
+                MessageBox.Show("Timed Out", "Error");
             }
-
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
