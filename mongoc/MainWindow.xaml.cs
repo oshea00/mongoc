e214118b0c5834d4474297ce16050b9be465472d
Mike OShea <oshea00@gmail.com>
2017-06-21 06:17:09 -0700

    Update MainWindow.xaml.cs

--- mongoc/MainWindow.xaml.cs
+++ mongoc/MainWindow.xaml.cs
@@ -7,9 +7,6 @@ using System.Windows;
 
 namespace mongoc
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         MongoServerHelper _server;
